15fac27427459272e464ad2b359a03ad45628245
zheng200412 <15350518876@163.com>
2025-12-21 16:05:03 +0800

    Vue工程项目生成

--- src/main/java/com/zzz/aicode/ai/AiCodeGeneratorService.java
+++ src/main/java/com/zzz/aicode/ai/AiCodeGeneratorService.java
@@ -2,10 +2,21 @@ package com.zzz.aicode.ai;
 
 import com.zzz.aicode.ai.model.HtmlCodeResult;
 import com.zzz.aicode.ai.model.MultiFileCodeResult;
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
 import reactor.core.publisher.Flux;
 
 public interface AiCodeGeneratorService {
+
+    /**
+     * 生成 Vue 项目代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成过程的流式响应
+     */
+    @SystemMessage(fromResource = "prompt/codegen-vue-project-system-prompt.txt")
+    Flux<String> generateVueProjectCodeStream(@MemoryId long appId, @UserMessage String userMessage);
 
     /**
      * 生成代码
