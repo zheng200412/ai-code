ea6805ca78b2d7005a9ade5fb0f8cdb8c605fa96
zheng200412 <15350518876@163.com>
2025-12-27 20:56:48 +0800

    性能优化

--- src/main/java/com/zzz/aicode/AiCodeApplication.java
+++ src/main/java/com/zzz/aicode/AiCodeApplication.java
@@ -4,9 +4,10 @@ import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStor
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-
+@EnableCaching
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 @MapperScan("com.zzz.aicode.mapper")
